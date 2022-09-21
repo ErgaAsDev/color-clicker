e7c186c32c76af2b2a5dcdee7475c114a2de2c6a
ErgaAsDev <108246514+ErgaAsDev@users.noreply.github.com>
2022-09-21 18:42:58 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const titleColor = document.querySelector("h4");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+  color.style.color = colors[randomNumber];
+  titleColor.style.color = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
